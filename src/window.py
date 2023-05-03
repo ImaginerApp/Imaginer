a1b82a8fdecf6345ddc9387049956dbda27a213d
0xMRTT <0xMRTT@proton.me>
2023-05-03 14:15:04 +0200

    src: add copyrights

--- src/window.py
+++ src/window.py
@@ -1,6 +1,6 @@
 # window.py
 #
-# Copyright 2023 Me
+# SPDX-FileCopyrightText: 2023 0xMRTT
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
